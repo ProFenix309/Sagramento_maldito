eccbbdfea69e82d8b7e93ff6a604fc77ea38fe20
Danijos <91360125+Danijos@users.noreply.github.com>
2025-08-13 17:32:14 -0500

    BasicMenu

---
+++ Assets/Scripts/UI/AudioSettings.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer; // Asignar AudioMixer en el Inspector
+    public Slider sliderMaster;
+    public Slider sliderMusica;
+    public Slider sliderAmbiente;
+
+    void Start()
+    {
+        // Puedes cargar valores guardados o poner valores por defecto
+        sliderMaster.onValueChanged.AddListener(SetMasterVolume);
+        sliderMusica.onValueChanged.AddListener(SetMusicaVolume);
+        sliderAmbiente.onValueChanged.AddListener(SetAmbienteVolume);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void SetMusicaVolume(float value)
+    {
+        audioMixer.SetFloat("MusicaVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void SetAmbienteVolume(float value)
+    {
+        audioMixer.SetFloat("AmbienteVolume", Mathf.Log10(value) * 20);
+    }
+}
+
---
+++ Assets/Scripts/UI/Menu script.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControlscript : MonoBehaviour
+{
+
+    public GameObject panelMenuPrincipal;
+    public GameObject panelConfiguracion;
+    public GameObject subpanelSonido;
+    public GameObject subpanelGraficos;
+    public GameObject panelCreditos;
+    Resolution[] resoluciones;
+    void Start()
+    {
+        ShowMenuPrincipal();
+        resoluciones = Screen.resolutions; // obtiene resoluciones disponibles
+    }
+
+    public void CambiarResolucionPorIndice(int indice)
+    {
+        Resolution res = resoluciones[indice];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+    public void Jugar()
+    {
+        SceneManager.LoadScene("NombreEscenaJuego");  // Cambia por el nombre real de tu escena
+    }
+    public void ShowMenuPrincipal()
+    {
+        panelMenuPrincipal.SetActive(true);
+        panelConfiguracion.SetActive(false);
+        panelCreditos.SetActive(false);
+    }
+
+    public void ShowConfiguracion()
+    {
+        panelMenuPrincipal.SetActive(false);
+        panelConfiguracion.SetActive(true);
+        panelCreditos.SetActive(false);
+
+       
+    }
+
+
+    public void ShowCreditos()
+    {
+        panelMenuPrincipal.SetActive(false);
+        panelConfiguracion.SetActive(false);
+        panelCreditos.SetActive(true);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Salir del juego");
+    }
+
+}
