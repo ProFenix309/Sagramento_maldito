d0af0a57d2667414b58ab4b3111f96c59e4a5cef
david <117487522+ProFenix309@users.noreply.github.com>
2025-08-20 17:30:13 -0500

    recolocando archivos

--- Assets/Scripts/Puerta/InteractiveDoor.cs
+++ Assets/Scripts/Puerta/InteractiveDoor.cs
@@ -10,7 +10,6 @@ public class InteractiveDoor : MonoBehaviour
     public bool checkOpen;
     public bool open;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         angle = transform.eulerAngles.y;
