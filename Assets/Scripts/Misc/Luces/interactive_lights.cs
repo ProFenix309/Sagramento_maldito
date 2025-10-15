f78e4cd7f2df891d871c85c5b384a420b0781af4
ProFenix309 <117487522+ProFenix309@users.noreply.github.com>
2025-10-15 17:26:09 -0500

    error light

--- Assets/Scripts/Misc/Luces/interactive_lights.cs
+++ Assets/Scripts/Misc/Luces/interactive_lights.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class interactive_lights : MonoBehaviour, Interactable
 {
     public Light Light;
+    public float lightRange;
+
+    public float onLight;
+    public float offLight;
+
+    private void Start()
+    {
+        Light = GetComponent<Light>();
+    }
     public void Interact()
     {
-
+        if (Light.intensity == onLight)
+        {
+            Light.intensity = offLight;
+        }
+        else
+        {
+            Light.intensity = onLight;
+        }
     }
+
+  
 }
