52eade907c2709e51395d2d1a522f807d9c31715
ProFenix309 <117487522+ProFenix309@users.noreply.github.com>
2025-11-21 17:40:59 -0500

    tutorial

--- Assets/4.Scripts/UI/Menu script.cs
+++ Assets/4.Scripts/UI/Menu script.cs
@@ -58,7 +58,7 @@ public class MenuControlscript : MonoBehaviour
     public void OnNewGameClicked()
     {
         TransitionPanel.gameObject.SetActive(true);
-        TransitionPanel.StartCoroutine(TransitionPanel.Transition("Alcantarilla"));
+        TransitionPanel.StartCoroutine(TransitionPanel.Transition("Casa"));
         DataPersistenceManager.instance.NewGame();
     }
 
