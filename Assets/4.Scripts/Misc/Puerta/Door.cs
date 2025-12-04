09eac9bab8a7d16009bad3a06877a3dfe2ebd270
ProFenix309 <117487522+ProFenix309@users.noreply.github.com>
2025-12-04 00:09:07 -0500

    door

--- Assets/4.Scripts/Misc/Puerta/Door.cs
+++ Assets/4.Scripts/Misc/Puerta/Door.cs
@@ -4,24 +4,20 @@ public class Door : MonoBehaviour, Interactable
 {
     [SerializeField] Transform door, open, close, target;
     [SerializeField] float speed;
-    [SerializeField] Audio_Manager sfx;
     private void Start()
     {
         open.SetParent(null);
         close.SetParent(null);
-        sfx = GameObject.Find("Audio Manager").GetComponent<Audio_Manager>();
     }
     public void Interact()
     {
         if (target == open)
         {
             target = close;
-            sfx.PlaySFX("Door");
         }
         else
         {
             target = open;
-            sfx.PlaySFX("Door");
         }
     }
     private void Update()
