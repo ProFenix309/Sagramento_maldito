461da2d2743aa640f6434e4815de84456387ed65
ProFenix309 <117487522+ProFenix309@users.noreply.github.com>
2025-12-09 14:25:04 -0500

    Casa y audios

--- Assets/4.Scripts/Misc/Puerta/Door.cs
+++ Assets/4.Scripts/Misc/Puerta/Door.cs
@@ -4,32 +4,35 @@ public class Door : MonoBehaviour, Interactable
 {
     [SerializeField] Transform door, open, close, target;
     [SerializeField] float speed;
-    AudioSource audioSource;
-    [SerializeField] AudioClip audioClip;
     private void Start()
     {
         open.SetParent(null);
         close.SetParent(null);
-        audioSource = GetComponent<AudioSource>();
     }
     public void Interact()
     {
         if (target == open)
         {
             target = close;
-            audioSource.PlayOneShot(audioClip);
+            PlaySFXSound();
         }
         else
         {
             target = open;
-            audioSource.PlayOneShot(audioClip);
+            PlaySFXSound();
         }
     }
+
     private void Update()
     {
         if (transform.rotation != target.rotation)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, speed * Time.deltaTime);
         }
+    }
+
+    private void PlaySFXSound()
+    {
+        AudioManager.Instance.PlaySFX3D("Door",transform.position);
     }
 }
