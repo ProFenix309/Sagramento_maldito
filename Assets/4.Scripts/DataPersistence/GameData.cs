ff95ce09825949794517c4b426249424c36fcc44
Adgelfox <miguelconcolor20@gmail.com>
2025-12-11 01:48:28 -0500

    position fix

--- Assets/4.Scripts/DataPersistence/GameData.cs
+++ Assets/4.Scripts/DataPersistence/GameData.cs
@@ -115,7 +115,7 @@ public class PlayerData
     [SerializeField] private Vector3 _playerPosition1 = new Vector3(2.16f, 3.05f, 3.95f);
 
     public Vector3 PlayerPosition2 { get => _playerPosition2; set => _playerPosition2 = value; }
-    [SerializeField] private Vector3 _playerPosition2 = new Vector3(0.1f, 5.81f, 6.92f);
+    [SerializeField] private Vector3 _playerPosition2 = new Vector3(0.25f, 5.685016f, 7.62f);
 
     public Vector3 PlayerPosition3 { get => _playerPosition3; set => _playerPosition3 = value; }
     [SerializeField] private Vector3 _playerPosition3 = new Vector3(2.16f, 3.05f, 3.95f);
--- Assets/4.Scripts/UnityEvents/SceneChange.cs
+++ Assets/4.Scripts/UnityEvents/SceneChange.cs
@@ -6,5 +6,6 @@ public class SceneChange : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 }
